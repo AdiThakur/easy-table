07208adc5bb9dbaab6783fd564b241cf2906d0d3
AdiThakur <55326963+AdiThakur@users.noreply.github.com>
2020-11-26 21:00:36 -0500

    Updated examples.html and examples.css

--- pub/examples.js
+++ pub/examples.js
@@ -1,27 +1,27 @@
 'use strict'
 const log = console.log
 
-const myTable = new EasyTable("myTable", "main", ['Model', 'Brand', 'Year'], true, 3)
+const step1 = new EasyTable("step1", "step1Table", ['Model', 'Brand', 'Year'], true, 3)
 
-myTable.appendRow(["Camry", "Toyota", "2000"])
-myTable.appendRow(["Civic", "Honda", "2005"])
-myTable.appendRow(["Accord", "Honda", "2001"])
-myTable.appendRow(["Mustang", "Ford", "1995"])
-myTable.appendRow(["Yaris", "Toyota", "1996"])
-myTable.appendRow(["Model-T", "Ford", "1102"])
-myTable.appendRow(["Enzo", "Ferrari", "1996"])
-myTable.appendRow(["458", "Ferrari", "1996"])
-myTable.appendRow(["458", "CarComp", "2020"])
+const step2 = new EasyTable("step2", "step2Table", ['Model', 'Brand', 'Year'], true, 3)
+step2.appendRow(["Camry", "Toyota", "2000"])
+step2.appendRow(["Civic", "Honda", "2005"])
+step2.appendRow(["Accord", "Honda", "2001"])
+step2.appendRow(["Gustang", "Ford", "1995"])
+step2.appendRow(["Yaris", "Toyota", "1996"])
+step2.appendRow(["Model-T", "Ford", "1102"])
+step2.insertRow(1, ["Enzo", "Ferrari", "1996"])
+step2.insertRow(1, ["458", "Ferrari", "1996"])
 
-// const enzo = myTable.popRow()
-// const model = myTable.popRow()
-// const civic = myTable.deleteRow(1)
+const step3 = new EasyTable("step3", "step3Table", ['Model', 'Brand', 'Year'], true, 3)
+step3.appendRow(["Camry", "Toyota", "2000"])
+step3.appendRow(["Civic", "Honda", "2005"])
+step3.appendRow(["Accord", "Honda", "2001"])
+step3.appendRow(["Gustang", "Ford", "1995"])
+step3.appendRow(["Yaris", "Toyota", "1996"])
+step3.appendRow(["Model-T", "Ford", "1102"])
+step3.insertRow(1, ["Enzo", "Ferrari", "1996"])
+step3.insertRow(1, ["458", "Ferrari", "1996"])
 
-const changeAccordYear = myTable.setRow(1, ["Civic", "Honda", "1970"])
-console.log(changeAccordYear)
-
-// const mustangCell = myTable.getCell(2, 0)
-// console.log(mustangCell)
-// mustangCell.style.cssText = "background-color: red; text-align: center"
-
-// https://salty-cove-11433.herokuapp.com/
+step3.setCell(5, 0, "Mustang")
+// https://salty-cove-11433.herokuapp.com/examples.html
